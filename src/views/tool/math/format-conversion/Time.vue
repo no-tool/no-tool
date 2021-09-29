d3e3664844f4f7188a9143f4800282872040ee69
youthred <xxooec@gmail.com>
2021-09-29 21:30:36 +0800

    time

--- src/views/tool/math/format-conversion/Time.vue
+++ src/views/tool/math/format-conversion/Time.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>大撒旦</div>
+  <div>
+    <v-text-field
+        label="格式"
+        v-model.trim="pattern"
+    ></v-text-field>
+    <v-text-field
+        label="格式化"
+        v-model.trim="datetimeFormat"
+    ></v-text-field>
+  </div>
 </template>
 
 <script>
+import moment from "moment"
+
 export default {
-  name: "Time"
+  name: "Time",
+  data: () => ({
+    pattern: 'YYYY-MM-DD HH:mm:ss',
+    datetimeFormat: ''
+  }),
+  mounted() {
+    const now = moment()
+    this.datetimeFormat = now.format(this.pattern)
+  }
 }
 </script>
 
